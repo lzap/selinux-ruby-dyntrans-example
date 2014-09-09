f588dcd41443981fb017b985ff5e1ab91073a6d7
Lukas Zapletal <lzap+git@redhat.com>
2014-09-09 13:36:59 +0200

    Changed macro

--- test.rb
+++ test.rb
@@ -5,7 +5,7 @@ domain = Selinux.getcon[1]
 puts "Started with domain #{domain}"
 
 #puts Selinux.setcon "unconfined_u:unconfined_r:test_subcommand_t:s0-s0:c0.c1023"
-puts Selinux.setcon "test_subcommand_t"
+puts Selinux.setcon "unconfined_u:unconfined_r:test_subcommand_t:s0"
 
 domain = Selinux.getcon[1]
 puts "After dynamic transition the domain is #{domain}"
